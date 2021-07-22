fa97f6f20b534127a09b221f0b6ac1bed6c0e197
akshatha.d <akshatha.d@pacewisdom.com>
2021-07-22 11:10:04 +0530

    Chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SunbirdVideoPlayerModule } from 'sunbird-video';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SunbirdVideoPlayerModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
